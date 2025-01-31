0d7d997dc8bf7751c680ca16b045574a8b3755cb
iliarusskikh <86166676+iliarusskikh@users.noreply.github.com>
2025-01-31 20:46:52 +0000

    c++17 hardware constructive interference size

---
+++ library_new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <new>
+
+int main()
+{
+    std::cout<<"hardware_constructive_interference_size "<<hardware_constructive_interference_size<< " bytes\n";
+    std::cout<<"hardware_constructive_interference_size "<<std::hardware_destructive_interference_size<< " bytes\n";
+    return 0;
+    
+}
+
