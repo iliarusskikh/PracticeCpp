5ad864e6773010f58ca84fa9ae563ed882bab230
iliarusskikh <justwill@protonmail.com>
2026-04-11 14:00:52 +0100

    quiz time

---
+++ challenge/quiz/q18.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+class A {
+public:
+  virtual void f() { std::cout << "A"; }
+};
+
+class B : public A {
+private:
+  void f() { std::cout << "B"; }
+};
+
+void g(A &a) { a.f(); }
+
+int main() {
+  B b;
+  g(b);
+}
+// Output: B
+//The call point here being a.f(), and the type of the expression is A&.
