db87c5198df78a8d36707e882e71651549338445
iliarusskikh <justwill@protonmail.com>
2026-04-16 18:16:13 +0100

    quiz time

---
+++ challenge/quiz/q145.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+struct E
+{
+  E() { std::cout << "1"; }
+  E(const E&) { std::cout << "2"; }
+  ~E() { std::cout << "3"; }
+};
+
+E f()
+{
+  return E();
+}
+
+int main()
+{
+  f();
+}
+// Output: 13
+//In f(), an E object is constructed, and 1 is printed. 
