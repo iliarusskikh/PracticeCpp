5a9f11a1697d9533f05204c74a54c26dc3af6edb
iliarusskikh <justwill@protonmail.com>
2026-04-29 20:25:29 +0100

    quiz time

---
+++ challenge/quiz/q8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class A {
+public:
+  virtual void f() { std::cout << "A"; }
+};
+
+class B : public A {
+public:
+  void f() { std::cout << "B"; }
+};
+
+void g(A a) { a.f(); }
+
+int main() {
+  B b;
+  g(b);
+}
+// Output: A
+// g(A a) takes an object of type A by value, not by reference or pointer. This means that A's copy constructor is called on the object passed to g() (no matter if the object we passed was of type B), and we get a brand new object of type A inside g(). This is commonly referred to as slicing.
+
