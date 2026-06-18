159307ac307682f526c9eef0d59f20f0f4d0ba99
iliarusskikh <justwill@protonmail.com>
2026-06-18 17:02:59 +0100

    quiz time

---
+++ challenge/quiz/q4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void f(float) { std::cout << 1; }
+void f(double) { std::cout << 2; }
+
+int main() {
+  f(2.5);
+  f(2.5f);
+}
+// Output: 21
+//The type of a floating point literal is double.
+
+
