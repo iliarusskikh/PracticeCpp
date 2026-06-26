95be348ce2a562bc8c0be40391ea95325fc3f96e
iliarusskikh <justwill@protonmail.com>
2026-06-26 16:47:56 +0100

    quiz time

---
+++ challenge/quiz/q142.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <type_traits>
+
+int main() {
+    std::cout << std::is_const_v<const int *>
+              << std::is_const_v<const int [1]>
+              << std::is_const_v<const int **>
+              << std::is_const_v<const int (*)[1]>
+              << std::is_const_v<const int *[1]>
+              << std::is_const_v<const int [1][1]>;
+}
+// Output: 010001
+//array is const, pointers are not const
