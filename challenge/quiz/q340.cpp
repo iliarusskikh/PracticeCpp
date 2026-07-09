8a2a8a0f6f137e379efe7a24f72991c845d64fdf
iliarusskikh <justwill@protonmail.com>
2026-07-09 16:15:51 +0100

    quiz time

---
+++ challenge/quiz/q340.cpp
@@ -0,0 +1,20 @@
+#include <future>
+#include <iostream>
+
+int main()
+{
+    try {
+        std::promise<int> p;
+        std::future<int> f1 = p.get_future();
+        std::future<int> f2 = p.get_future();
+        p.set_value(1);
+        std::cout << f1.get() << f2.get();
+    } catch(const std::exception& e)
+    {
+        std::cout << 2;
+    }
+}
+// Output: 2
+//You can only get a future from a promise once. Trying to get it again throws future_error.
+
+
